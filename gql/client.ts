9982d9669bbfaabc18b51d060750fe17fdd1dbd9
halykon <sicstar21@gmail.com>
2022-01-05 23:05:38 +0100

    Initial

---
+++ gql/client.ts
@@ -0,0 +1,7 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+
+export const apolloClient = new ApolloClient({
+  uri: process.env.GRAPHQL_ENDPOINT!,
+  cache: new InMemoryCache()
+})
+
