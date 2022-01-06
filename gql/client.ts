e44ba2c5bac119947044465e829eb1077460546d
halykon <sicstar21@gmail.com>
2022-01-06 02:47:45 +0100

    + gql client

--- gql/client.ts
+++ gql/client.ts
@@ -1,7 +1,40 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from '@apollo/client'
+import { onError } from '@apollo/client/link/error'
+import { toast } from 'react-toastify'
+
+
+const anilistLink = ApolloLink.from([
+  onError(({ response }) => {
+    response?.errors?.forEach(({ message }) => {
+      toast(`Error while getting data from AniList: ${message}`, { type: 'error' })
+    })
+
+    // this is not a hack: https://www.apollographql.com/docs/react/data/error-handling/#ignoring-errors
+    if (response?.errors) response.errors = undefined
+  }),
+  new HttpLink({
+    uri: 'graphql.anilist.co',
+  }),
+])
+
+const backendLink = ApolloLink.from([
+  onError(({ response }) => {
+    response?.errors?.forEach(({ message }) => {
+      toast(`Error while getting data the API: ${message}`, { type: 'error' })
+    })
+
+    if (response?.errors) response.errors = undefined
+  }),
+  new HttpLink({
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+  }),
+])
+
 
 export const apolloClient = new ApolloClient({
-  uri: process.env.GRAPHQL_ENDPOINT!,
-  cache: new InMemoryCache()
+  link: ApolloLink.split(
+    operation => operation.getContext().client === 'anilist', anilistLink, backendLink,
+  ),
+  cache: new InMemoryCache(),
 })
 
