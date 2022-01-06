8e76b41f6f2bec746151b75ec03c550bfab8aca4
halykon <sicstar21@gmail.com>
2022-01-06 02:57:55 +0100

    + toastify css, theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,12 @@ import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { ToastContainer } from 'react-toastify'
 import { apolloClient } from '../gql/client'
+import 'react-toastify/dist/ReactToastify.css'
+import theme from '../theme'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <ApolloProvider client={apolloClient}>
         <Component {...pageProps}/>
         <ToastContainer/>
